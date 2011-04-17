3b18ea8d5f3591c308fd65b3cd733480d4fb6fac
Paul Yang <zapionator@gmail.com>
2011-04-17 20:42:23 +0800

    new file:   plurkUtil.py 	new file:   properties.py
    go for it

---
+++ plurkUtil.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+def new_plurk():
+    pass
+
+def reply():
+    pass
+
---
+++ properties.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+class properties:
+    def __init__(self, account='tmstaf', pswd, api_key):
+        pass
+
+
